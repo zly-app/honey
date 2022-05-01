13de6f58beaf203263427002369c2e95ebac98c5
zlyuancn <zlyuancn@163.com>
2022-05-01 08:22:57 +0800

    HoneyPlugin检查启动状态

--- zapp_plugin/honey.go
+++ zapp_plugin/honey.go
@@ -2,6 +2,7 @@ package honey
 
 import (
 	"sync"
+	"sync/atomic"
 
 	"github.com/zly-app/zapp"
 	"github.com/zly-app/zapp/core"
@@ -17,7 +18,8 @@ import (
 type HoneyPlugin struct {
 	app    core.IApp
 	conf   *config.Config
-	isInit bool // 是否完成初始化
+	isInit bool  // 是否完成初始化
+	state  int32 // 启动状态 0未启动, 1已启动
 
 	rotate      rotate.IRotator // 旋转器
 	rotateGPool core.IGPool     // 用于处理同时旋转的协程池
@@ -42,10 +44,15 @@ func (h *HoneyPlugin) Init() {
 	h.isInit = true
 }
 
+func (h *HoneyPlugin) isStart() bool {
+	return atomic.LoadInt32(&h.state) == 1
+}
+
 func (h *HoneyPlugin) Start() {
 	if !h.isInit {
 		return
 	}
+	atomic.StoreInt32(&h.state, 1)
 
 	// 启动上报者
 	h.MakeReporter()
@@ -53,6 +60,7 @@ func (h *HoneyPlugin) Start() {
 }
 
 func (h *HoneyPlugin) Close() {
+	atomic.StoreInt32(&h.state, 0)
 	if !h.isInit {
 		return
 	}
@@ -67,7 +75,7 @@ func (h *HoneyPlugin) Close() {
 func (h *HoneyPlugin) LogInterceptorFunc(ent *zapcore.Entry, fields []zapcore.Field) (cancel bool) {
 	log := log_data.MakeLogData(ent, fields)
 	h.rotate.Add(log)
-	return h.conf.StopLogOutput
+	return h.conf.StopLogOutput && h.isStart() // 设置了拦截并且在服务启动后才允许拦截
 }
 
 var honey *HoneyPlugin
