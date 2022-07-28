438db025c53e1c2eb0d243a031b7e7ec7b0fc95c
zlyuanzhang <zlyuanzhang@tencent.com>
2022-07-28 19:29:58 +0800

    去掉无用的配置

--- input/http/config.go
+++ input/http/config.go
@@ -7,8 +7,6 @@ const (
 	defPushPath = "/push"
 	// 默认post允许最大数据大小(32M)
 	defaultPostMaxMemory = 32 << 20
-	// 默认序列化器名
-	defaultSerializer = "msgpack"
 )
 
 type Config struct {
@@ -16,7 +14,6 @@ type Config struct {
 	Bind          string // 监听地址, 示例: :8080
 	PushPath      string // 推送路径
 	PostMaxMemory int64  // post允许客户端传输最大数据大小, 单位字节
-	Serializer    string // 序列化器名
 	AuthToken     string // 验证token, 如果设置, 客户端请求header必须带上 token={AuthToken}, 如 token=myAuthToken
 }
 
@@ -33,9 +30,6 @@ func (c *Config) Check() error {
 	}
 	if c.PostMaxMemory < 1 {
 		c.PostMaxMemory = defaultPostMaxMemory
-	}
-	if c.Serializer == "" {
-		c.Serializer = defaultSerializer
 	}
 	return nil
 }
